b53116138878bbeec8950becaebc33a9a41ad1b6
Daniel Nicodemo <dan@showcaseidx.com>
2018-10-25 12:33:29 -0400

    Setup the variables and console statements

--- magic8ball.js
+++ magic8ball.js
@@ -1,1 +1,6 @@
-//Magic 8 Ball practice project
+let userName = 'Dan';
+let userQuestion = '\'Will I get a job from these silly exercises?\' ';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+console.log('It appears as though ' + userName + ' has a question: ' + userQuestion);
